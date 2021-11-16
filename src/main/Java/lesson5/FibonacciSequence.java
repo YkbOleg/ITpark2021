a31354406a768f953e5930eb51de616b9b43ba69
Ykbol <Ykb.Oleg@gmail.com>
2021-11-16 19:38:43 +0500

    add class FibonacciSequence

--- src/main/Java/lesson5/FibonacciSequence.java
+++ src/main/Java/lesson5/FibonacciSequence.java
@@ -1,8 +1,50 @@
 package lesson5;
 
+import java.util.Scanner;
+
 public class FibonacciSequence {
 
     public static void main(String[] args) {
+        System.out.print("Введите n-ый член последовательности Фибоначчи: ");
+        int n = input();
+        System.out.println(sequenceFibonacci(n));
+    }
 
+    /**
+     * Ввад данных
+     * @return Возврат введённого значения
+     */
+    public static int input() {
+        Scanner in = new Scanner(System.in);
+        while (!in.hasNextInt()){
+            System.out.print("Вы ввели строку, введите целое число: ");
+            in.next();
+        }
+        return in.nextInt();
+    }
+
+    /**
+     * Вычисление числа Фибоначчи
+     * @param n - порядковый номер числа Фибонначи
+     * @return Число Фибоначи
+     */
+    public static int sequenceFibonacci(int n) {
+        int fibonacciNumber = 0;
+        while (n < 1){
+            System.out.print("Введите целое число больше 0: ");
+            n = FibonacciSequence.input();
+        }
+
+        if (n != 2){
+            for(int i = 2, c = 0, j = 1; i < n; i++){
+                fibonacciNumber = c + j;
+                c = j;
+                j = fibonacciNumber;
+            }
+        }
+        else {
+            return 1;
+        }
+        return fibonacciNumber;
     }
 }
