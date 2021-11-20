68af83ca49582f9ed97da21fd82ad81b9d122a8f
Ykbol <Ykb.Oleg@gmail.com>
2021-11-20 10:26:00 +0500

    add method sequenceFibonacci()

--- src/main/Java/lesson6/UsingArrays.java
+++ src/main/Java/lesson6/UsingArrays.java
@@ -1,5 +1,7 @@
 package lesson6;
 
+import lesson5.FibonacciSequence;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -29,6 +31,10 @@ public class UsingArrays {
         System.out.println("Максимальное значение в массиве: " + maxValue(randomNumberArray));
         System.out.println("Среднее значение массива: " + averageValue(randomNumberArray));
 
+        System.out.print("Введите n-ый член последовательности Фибоначчи: ");
+        int n = input();
+        System.out.print(n + " член последовательности Фибоначчи: ");
+        System.out.println(sequenceFibonacci(n));
     }
 
     /**
@@ -38,12 +44,17 @@ public class UsingArrays {
     public static int input() {
         Scanner in = new Scanner(System.in);
         while (!in.hasNextInt()){
-            System.out.print("Вы ввели строку, введите целое число: ");
+            System.out.print("Вы ввели строку или дробное число, введите целое число: ");
             in.next();
         }
         return in.nextInt();
     }
 
+    /**
+     * Нахождение минимального числа в массиве
+     * @param array массив целых чисел
+     * @return минимальное число в массиве
+     */
     public static int minValue(int[] array) {
         int minV = array[0];
         for (int i : array){
@@ -54,8 +65,13 @@ public class UsingArrays {
         return minV;
     }
 
+    /**
+     * Нахождение максимального числа в массиве
+     * @param array массив целых чисел
+     * @return максимальное число в массиве
+     */
     public static int maxValue(int[] array) {
-        int maxV = array[1];
+        int maxV = array[0];
         for (int i : array) {
             if (maxV < i) {
                 maxV = i;
@@ -64,11 +80,46 @@ public class UsingArrays {
         return maxV;
     }
 
+    /**
+     * Нахождение средненго значения по массиву
+     * @param array массив целых чисел
+     * @return среднее значение по массиву
+     */
     public static double averageValue(int[] array) {
         double averageV = 0;
         for (int i : array){
             averageV += i;
         }
         return averageV/array.length;
+    }
+
+    /**
+     * Расчёт числа Фибоначчи
+     * @param n порядковый номер числа Фибоначчи
+     * @return Число Фибоначчи в позиции n
+     */
+    public static int sequenceFibonacci(int n) {
+        int temp = 20;   //количество текущих значений в массиве arrayFibonacci
+        while (n <= 0){
+            System.out.print("Введите целое число больше 0: ");
+            n = FibonacciSequence.input();
+        }
+
+        int[] arrayFibonacci = new int[temp];
+        arrayFibonacci[0] = 0;
+        arrayFibonacci[1] = 1;
+        for (int i = 2; i < arrayFibonacci.length; i++) {
+            arrayFibonacci[i] = arrayFibonacci[i - 1] + arrayFibonacci[i - 2];
+        }
+
+        if (n > temp){
+            int[] tempArrayFibonacci = new int[n];  //Новый массив последовательности Фибоначчи
+            System.arraycopy(arrayFibonacci, 0, tempArrayFibonacci, 0,arrayFibonacci.length);
+            for (int i = temp; i < n; i++) {
+                tempArrayFibonacci[i] = tempArrayFibonacci[i - 1] + tempArrayFibonacci[i - 2];
+            }
+            return tempArrayFibonacci[n-1];
+        }
+        return arrayFibonacci[n-1];
     }
 }
