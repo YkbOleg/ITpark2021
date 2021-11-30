1c20f1b3d349a7a7016c013f83ab6b993e35e47e
Ykbol <Ykb.Oleg@gmail.com>
2021-11-30 23:03:15 +0500

    Upgrade class Matrix and class MatrixRun

--- src/main/Java/lesson8/Matrix.java
+++ src/main/Java/lesson8/Matrix.java
@@ -1,6 +1,8 @@
 package lesson8;
 
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Matrix {
 
@@ -8,17 +10,21 @@ public class Matrix {
     private int column;
     private final int[][] matrix;
 
+    //геттеры
     public int[][] getMatrix() {
         return matrix;
     }
+    public int getRows() { return rows; }
+    public int getColumn() { return column; }
 
+    //Конструктор по умолчанию
     public Matrix() {
-        this.matrix = new int[rows][column];
         this.rows = 2;
         this.column = 2;
-        initializationMatrix(this.matrix);
+        this.matrix = new int[rows][column];
     }
 
+    //Конструктор (int, int)
     protected Matrix(int m, int n) {
         this.matrix = new int[m][n];
     }
@@ -40,7 +46,6 @@ public class Matrix {
      * @return Возврат введённого значения
      */
     public static int input() {
-        //Scanner scanner = new Scanner(System.in);
         int val = checkForString();
         while (val <= 0) {
             if (val < 0) {
@@ -51,7 +56,6 @@ public class Matrix {
             System.out.print("Введите целое число больше 0: ");
             val = input();
         }
-
         return val;
     }
 
@@ -74,7 +78,7 @@ public class Matrix {
      * @param twoMatrix Вторая матрица
      * @return Результат сравнения
      */
-    public static boolean compareMatrix(int[][] oneMatrix, int[][] twoMatrix){
+    private static boolean compareMatrix(int[][] oneMatrix, int[][] twoMatrix){
         return oneMatrix.length == twoMatrix.length && oneMatrix[0].length == twoMatrix[0].length;
     }
 
@@ -84,9 +88,8 @@ public class Matrix {
      * @param matrixTwo вторая матрица
      * @return Матрица являющаяся суммой двух матриц
      */
-    public static int[][] sumMatrix(int[][] matrixOne, int[][] matrixTwo) {
+    private static int[][] sumMatrix(int[][] matrixOne, int[][] matrixTwo) {
         Matrix matrixSum = new Matrix(matrixOne.length, matrixOne[0].length);
-        //int[][] matrixSum = new int[matrixOne.length][matrixOne[0].length];
         for(int i = 0; i < matrixSum.matrix.length; i++){
             for(int j = 0; j < matrixSum.matrix[0].length; j++){
                 matrixSum.matrix[i][j] = matrixTwo[i][j] + matrixOne[i][j];
@@ -101,7 +104,7 @@ public class Matrix {
      * @param matrixTwo вторая матрица
      * @return Матрица являющаяся разностью двух матриц
      */
-    public static int[][] differenceMatrix(int[][] matrixOne, int[][] matrixTwo) {
+    private static int[][] differenceMatrix(int[][] matrixOne, int[][] matrixTwo) {
         Matrix matrixDifference = new Matrix(matrixOne.length, matrixOne[0].length);
         for(int i = 0; i < matrixDifference.matrix.length; i++){
             for(int j = 0; j < matrixDifference.matrix[0].length; j++){
@@ -117,7 +120,7 @@ public class Matrix {
      * @param multiplier множетель
      * @return Матрица - результат умножения матрицы на число
      */
-    public static int[][] multiplicationByNumber(int[][] matrix, int multiplier){
+    private static int[][] multiplicationByNumber(int[][] matrix, int multiplier){
         Matrix multiplicationMatrix = new Matrix(matrix.length, matrix[0].length);
         for(int i = 0; i < multiplicationMatrix.matrix.length; i++){
             for(int j = 0; j < multiplicationMatrix.matrix[0].length; j++){
@@ -127,16 +130,71 @@ public class Matrix {
         return multiplicationMatrix.matrix;
     }
 
-    public static int[][] multiplicationMatrix(int[][] matrixOne, int[][] matrixTwo){
+    /**
+     * Умножение матриц
+     * @param matrixOne первая матрица
+     * @param matrixTwo вторая матрица
+     * @return матрица - результат умножения двух матриц
+     */
+    private static int[][] multiplicationMatrix(int[][] matrixOne, int[][] matrixTwo){
         Matrix matrixMultiplication = new Matrix(matrixOne.length, matrixTwo[0].length);
-        for(int i = 0; i < matrixOne.length; i++) {         // rows from m1
-            for(int j = 0; j < matrixTwo[0].length; j++) {     // columns from m2
-                for(int k = 0; k < matrixOne[0].length; k++) { // columns from m1
+        for(int i = 0; i < matrixOne.length; i++) {
+            for(int j = 0; j < matrixTwo[0].length; j++) {
+                for(int k = 0; k < matrixOne[0].length; k++) {
                     matrixMultiplication.matrix[i][j] += matrixOne[i][k] * matrixTwo[k][j];
                 }
             }
         }
-
         return matrixMultiplication.matrix;
+    }
+
+    /**
+     * Метод деиствия над матрицами
+     * @param matrixOne первая матрица
+     * @param matrixTwo вторая матрица
+     */
+    public static void operationsMatrix(int[][] matrixOne, int[][] matrixTwo){
+        String run;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Выберите операцию на матрицами:");
+            System.out.println("0 - Завершить работу программы");
+            System.out.println("1 - сложение матриц");
+            System.out.println("2 - вычитание матриц");
+            System.out.println("3 - умножение матриц");
+            System.out.println("4 - умножение матрицы на число");
+            run = scanner.next();
+        }
+        while (!Objects.equals(run, "0")
+                && !Objects.equals(run, "1")
+                && !Objects.equals(run, "2")
+                && !Objects.equals(run, "3")
+                && !Objects.equals(run, "4"));
+
+        switch (run){
+            case "0": break;
+            case "1":
+                if(compareMatrix(matrixOne, matrixTwo)){
+                    System.out.println("Сумма матриц: " + Arrays.deepToString(sumMatrix(matrixOne, matrixTwo)));
+                }
+                else System.out.println("Разная размерность матриц, вычисление не возможно!");
+                break;
+            case "2":
+                if(compareMatrix(matrixOne, matrixTwo)) {
+                    System.out.println("Разность матриц: " + Arrays.deepToString(differenceMatrix(matrixOne, matrixTwo)));
+                }
+                else System.out.println("Разная размерность матриц, вычисление не возможно!");
+                break;
+            case "3":
+                System.out.println("Умножение матриц: " + Arrays.deepToString(multiplicationMatrix(matrixOne, matrixTwo)));
+                break;
+            case "4":
+                System.out.println("Введите целое число для умножения");
+                int n = checkForString();
+                System.out.println("Умножение матриц на число: " + Arrays.deepToString(multiplicationByNumber(matrixOne, n)));
+                System.out.println("Умножение матриц на число: " + Arrays.deepToString(multiplicationByNumber(matrixTwo, n)));
+                break;
+        }
     }
 }
